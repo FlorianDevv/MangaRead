1e31e02a75bde5b349eea4e287947ab3d2dd1164
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-03-24 12:08:20 +0100

    Add DynamicBlur component for image processing

---
+++ app/components/dynamicBlur.tsx
@@ -0,0 +1,46 @@
+import fs from "fs/promises";
+import Image from "next/image";
+import path from "path";
+import { getPlaiceholder } from "plaiceholder";
+import React from "react";
+
+const DynamicBlur: React.FC<{ src: string; alt: string }> = async ({
+  src,
+  alt,
+}) => {
+  const imagePath = path.join(process.cwd(), "public", src); // Assurez-vous que le chemin est correct
+
+  let file;
+  try {
+    file = await fs.readFile(imagePath);
+  } catch (err) {
+    console.error(`Failed to read image file: ${err}`);
+    return null; // Or handle the error in another way
+  }
+
+  let base64;
+  try {
+    base64 = await getPlaiceholder(file);
+  } catch (error) {
+    console.error("Failed to process image with plaiceholder:", error);
+    return null; // Or handle the error in another way
+  }
+
+  return (
+    <div className="">
+      <Image
+        src={src}
+        alt={alt}
+        style={{ objectFit: "cover" }}
+        quality={1}
+        fill
+        sizes="90vw"
+        placeholder="blur"
+        blurDataURL={base64.base64} // Convert base64 object to string
+        className="transition-all duration-500 ease-in-out transform "
+      />
+    </div>
+  );
+};
+
+export default DynamicBlur;
