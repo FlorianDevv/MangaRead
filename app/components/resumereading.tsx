53eaf5c23bfb7f795ac3aa50dd965dad0bc17b25
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-03-26 11:35:18 +0100

    Refactor ResumeReading component layout

--- app/components/resumereading.tsx
+++ app/components/resumereading.tsx
@@ -31,13 +31,13 @@ export default function ResumeReading() {
       <h2 className="w-full text-center text-3xl mb-4 mt-6">
         Reprendre la lecture
       </h2>
-      <div className="flex flex-nowrap justify-start items-center overflow-x-auto mx-8">
-        {state.map((mangaInfo, index) => (
-          <Link
-            key={index}
-            href={`/manga/${mangaInfo.manga}/${mangaInfo.volume}/`}
-          >
-            <div className="flex flex-col items-stretch m-2 bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:opacity-80">
+      <div className="flex flex-col items-stretch m-4 bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out transform hover:opacity-80">
+        <div className="flex flex-nowrap justify-center items-center overflow-x-auto">
+          {state.map((mangaInfo, index) => (
+            <Link
+              key={index}
+              href={`/manga/${mangaInfo.manga}/${mangaInfo.volume}/`}
+            >
               <div className="relative h-32 sm:h-48 md:h-64 w-32 sm:w-48 md:w-64 flex-shrink-0">
                 <Image
                   src={`/${mangaInfo.manga}/Tome 01/01-001.webp`}
@@ -45,7 +45,7 @@ export default function ResumeReading() {
                   quality={1}
                   fill
                   style={{ objectFit: "cover" }}
-                  sizes="70vw"
+                  sizes="90vw"
                   className="transition-all duration-500 ease-in-out transform 5  "
                 />
               </div>
@@ -56,11 +56,10 @@ export default function ResumeReading() {
                   Page {mangaInfo.page}
                 </p>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
-      <hr className="my-8" />
     </div>
   );
 }
