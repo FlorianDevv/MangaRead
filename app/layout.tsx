3f929a4258f974c7c5c97138e3d8014b0cfef8e1
Florian PICHON <fpichon@protechnologies.fr>
2024-04-05 14:59:31 +0200

    Add Sonner component for displaying toasts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/navbar";
@@ -53,6 +54,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Toaster />
       </body>
     </html>
   );
