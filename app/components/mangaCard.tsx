2a63560f0fd57959e5eb5fb42ca0c2c066082b33
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-04-14 15:41:21 +0200

    Refactor MangaCard component to use Link from "next/link" and update JSX structure

--- app/components/mangaCard.tsx
+++ app/components/mangaCard.tsx
@@ -1,19 +1,24 @@
+// MangaCard.tsx
+import Link from "next/link";
 import DynamicBlur from "./dynamicBlur";
-
 export default function MangaCard({ mangaName }: { mangaName: string }) {
   return (
-    <div className="flex flex-col rounded-lg overflow-hidden shadow-lg hover:shadow-2xl ease-in-out transform group hover:scale-105 transition-transform duration-300">
-      <div className="relative h-32 sm:h-48 md:h-64 w-32 sm:w-48 md:w-64 shine">
-        <DynamicBlur
-          src={`/${mangaName}/Tome 01/01-001.webp`}
-          alt={mangaName}
-        />
-      </div>
-      <div className="p-2">
-        <h1 className="text-sm text-center text-white transition-colors duration-300 ease-in-out group-hover:text-red-500 break-words">
-          {mangaName}
-        </h1>
-      </div>
+    <div className="flex flex-col items-stretch rounded-lg overflow-hidden shadow-lg hover:shadow-2xl ease-in-out transform group hover:scale-105 transition-transform duration-300 m-2 w-full">
+      <Link key={mangaName} href={`/manga/${mangaName}`}>
+        <div className="flex flex-col items-stretch">
+          <div className="relative h-48 sm:h-64 md:h-72 lg:h-96 w-full">
+            <DynamicBlur
+              src={`/${mangaName}/Tome 01/01-001.webp`}
+              alt={mangaName}
+            />
+          </div>
+          <div className="p-2">
+            <h1 className="text-xs sm:text-sm md:text-base text-center text-white transition-colors duration-300 ease-in-out group-hover:text-red-500 break-words">
+              {mangaName}
+            </h1>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 }
