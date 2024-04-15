7fa3e32044dc422213b764946a515b7a3d21f516
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-04-15 22:53:07 +0200

    Refactor images rendering in MangaCard component

--- app/components/mangaCard.tsx
+++ app/components/mangaCard.tsx
@@ -6,12 +6,10 @@ export default function MangaCard({ mangaName }: { mangaName: string }) {
     <div className="flex flex-col items-stretch rounded-lg overflow-hidden shadow-lg hover:shadow-2xl ease-in-out transform group hover:scale-105 transition-transform duration-300 m-2 w-full">
       <Link key={mangaName} href={`/manga/${mangaName}`}>
         <div className="flex flex-col items-stretch">
-          <div className="relative h-48 sm:h-64 md:h-72 lg:h-96 w-full">
-            <DynamicBlur
-              src={`/${mangaName}/Tome 01/01-001.webp`}
-              alt={mangaName}
-            />
-          </div>
+          <DynamicBlur
+            src={`/${mangaName}/Tome 01/01-001.webp`}
+            alt={mangaName}
+          />
           <div className="p-2">
             <h1 className="text-xs sm:text-sm md:text-base text-center text-white transition-colors duration-300 ease-in-out group-hover:text-red-500 break-words">
               {mangaName}
