72aea03797811718a113ed753cf13f5042d56417
FlorianDevv <118461513+FlorianDevv@users.noreply.github.com>
2024-07-04 19:22:03 +0200

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,21 @@
 import withSerwistInit from "@serwist/next";
 
 const withSerwist = withSerwistInit({
-	swSrc: "app/sw.ts",
-	swDest: "public/sw.js",
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
 });
 
 const nextConfig = {
-	images: {
-		formats: ["image/webp"],
-	},
-	env: {
-		DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE,
-		MAX_QUALITY: process.env.MAX_QUALITY,
-		MIN_QUALITY: process.env.MIN_QUALITY,
-		DEFAULT_QUALITY: process.env.DEFAULT_QUALITY,
-	},
+  images: {
+    formats: ["image/webp"],
+  },
+  env: {
+    DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE,
+    MAX_QUALITY: process.env.MAX_QUALITY,
+    MIN_QUALITY: process.env.MIN_QUALITY,
+    DEFAULT_QUALITY: process.env.DEFAULT_QUALITY,
+  },
 };
 
 export default withSerwist(nextConfig);
